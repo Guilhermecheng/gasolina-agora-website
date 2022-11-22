67ee8bb56d3af3e3f5df1bb1ec32d8a9c5ed3064
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2022-11-22 00:39:30 -0300

    api Resp for test

---
+++ src/utils/apiResp.ts
@@ -0,0 +1,31 @@
+
+export const apiResp = {
+	"etanol_price": 3.7,
+	"gasoline_price": 4.98,
+	"max_price_station_details": {
+		"address": "Rua Jose Saboia Livreiro, 1410 Altamira",
+		"data_id": "GASOLINA COMUM\\BRASIL\\NORDESTE\\CEARA\\CRATEUS",
+		"distributor": "SP",
+		"name": "Alberi G. de Araujo"
+	},
+	"min_price_station_details": {
+		"address": "",
+		"company": "",
+		"data_id": "GASOLINA COMUM\\BRASIL\\SUDESTE\\SAO PAULO\\SAO CAETANO DO SUL",
+		"distributor": ""
+	},
+	"result": {
+		"data_id": "GASOLINA COMUM\\BRASIL",
+		"deviation": 0.299,
+		"end_date": "2022-11-05",
+		"initial_date": "2022-10-30",
+		"measure_unit": "R$/l",
+		"price_average": 4.98,
+		"price_max": 6.99,
+		"price_min": 4.26,
+		"product_name": "GASOLINA COMUM",
+		"scope": "country",
+		"scope_description": "BRASIL",
+		"stations": 2368
+	}
+}
